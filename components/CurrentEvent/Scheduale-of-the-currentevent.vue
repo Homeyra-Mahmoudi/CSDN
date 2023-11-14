6cd416bc972be9f2922d490654a2203e28486ca4
Homeyra-Mahmoudi <homeyra.mahmoudi@mail.polimi.it>
2023-11-14 19:56:56 +0100

    static site

--- components/CurrentEvent/Scheduale-of-the-currentevent.vue
+++ components/CurrentEvent/Scheduale-of-the-currentevent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="lg:container mx-auto md:pl-44 md:pr-44">
         <div class="sm:grid sm:py-6 bg-pink-950">
-            <p class="font-Roboto text-5xl pb-8 text-white text-center italic font-thin">INCONTRO - DIBATTITO</p>
+            <p class="font-Roboto text-5xl pb-8 pt-8 text-white text-center italic font-thin">INCONTRO - DIBATTITO</p>
             <p class="font-Roboto text-5xl text-white text-center  font-normal">NUOVE BUONE PRASSI DELLA SEZIONE LAVORO E DEONTOLOGIA</p>
         </div>
         <div class="sm:grid sm:py-6 bg-pink-950">
@@ -11,23 +11,51 @@ Avvocatura Torinese
 Palazzo Capris
 Via Santa Maria n. 1 - Torino</p>
         </div>
-        <div class="grid lg:grid-cols-2 gap-4 bg-pink-950 py-6">
+        <div class="grid lg:grid gap-4 bg-pink-950 py-6">
         <div class="sm:px-14">
-            <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 10.15</p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left">PRESIEDONO:</p>
+            <!-- <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 10.15</p>
             <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Registrazione partecipanti</p>
             <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 10.45</p>
-            <p class="font-Roboto font-bold text-3xl italic text-white text-left">Apertura dei lavori</p>
-            <p class="font-Roboto font-thin text-2xl italic text-white text-left sm:pb-10">Introduce e coordina <span class="font-bold">Dott.ssa Daniela Paliaga</span> Presidente CSDN Sezione Piemonte e Presidente della Sez. Lavoro Tribunale di Torino</p>
-            <p class="font-Roboto font-thin text-2xl italic text-white text-left sm:pb-10"><span class="font-bold">Avv.ta Mirella Caffaratti </span>del Foro di Torino, “La malattia come fattore tipico di discriminazione” </p>
-            <p class="font-Roboto font-thin text-2xl italic text-white text-left sm:pb-14"><span class="font-bold">Prof. Domenico Garofalo</span>, Ordinario di Diritto del Lavoro Università di Bari “La risoluzione del rapporto di lavoro per malattia nel prisma degli accomodamenti ragionevoli”</p>
-            <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 12.30 - 12.45 </p>
+            <p class="font-Roboto font-bold text-3xl italic text-white text-left">Apertura dei lavori</p> -->
+            <p class="font-Roboto font-thin text-xl italic text-white text-left "><span class="font-bold">Dr.ssa CLOTILDE FIERRO</span>, Presidente Corte d'Appello Sezione Lavoro di Torino</p>
+            <p class="font-Roboto font-thin text-xl italic text-white text-left "><span class="font-bold">Avv.to GIOVANNA PACCHIANA PARRAVICINI</span>, Presidente AGI Piemonte Valle d'Aosta</p>
+            <p class="font-Roboto font-thin text-xl italic text-white text-left "><span class="font-bold">Dott.ssa DANIELA PALIAGA,</span>, Presidente Centro Studi Sezione Piemonte e Presidente della sezione lavoro del Tribunale di Torino</p>
+            <!-- <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 12.30 - 12.45 </p>
             <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Prenotazione interventi </p>
             <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 12.45 - 13.45 </p>
             <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Light lunch</p>
             <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 14</p>
-            <p class="font-Roboto font-thin text-2xl italic text-white text-left sm:pb-14"><span class="font-bold">Dott.ssa Lucia Tria</span>, Presidente di sezione Corte di Cassazione Sez. Lavoro “Malattia e disabilità del lavoratore davanti alla Corte di Cassazione”</p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left sm:pb-14"><span class="font-bold">Dott.ssa Lucia Tria</span>, Presidente di sezione Corte di Cassazione Sez. Lavoro “Malattia e disabilità del lavoratore davanti alla Corte di Cassazione”</p> -->
         </div>
         <div class="sm:px-14">
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left">RELAZIONA:</p>
+            <!-- <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 10.15</p>
+            <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Registrazione partecipanti</p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 10.45</p>
+            <p class="font-Roboto font-bold text-3xl italic text-white text-left">Apertura dei lavori</p> -->
+            <p class="font-Roboto font-thin text-xl italic text-white text-left "><span class="font-bold">Avv.to Paolo BERTI,</span>, Consigliere dell’Ordine degli Avvocati di Torino “Le buone prassi e la deontologia dell’Avvocato”</p>
+            <!-- <p class="font-Roboto font-thin text-xl italic text-white text-left "><span class="font-bold">Avv.to GIOVANNA PACCHIANA PARRAVICINI</span>, Presidente AGI Piemonte Valle d'Aosta</p>
+            <p class="font-Roboto font-thin text-xl italic text-white text-left "><span class="font-bold">Dott.ssa DANIELA PALIAGA,</span>, Presidente Centro Studi Sezione Piemonte e Presidente della sezione lavoro del Tribunale di Torino</p> -->
+            <!-- <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 12.30 - 12.45 </p>
+            <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Prenotazione interventi </p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 12.45 - 13.45 </p>
+            <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Light lunch</p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 14</p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left sm:pb-14"><span class="font-bold">Dott.ssa Lucia Tria</span>, Presidente di sezione Corte di Cassazione Sez. Lavoro “Malattia e disabilità del lavoratore davanti alla Corte di Cassazione”</p> -->
+        </div>
+        <div class="sm:px-14 sm:py-14">
+            <p class="font-Roboto font-thin text-xl italic text-white text-left ">Seguirà il consueto buffet natalizio per lo scambio di auguri.</p>
+            <!-- <p class="font-Roboto font-thin text-xl italic text-white text-left "><span class="font-bold">Avv.to GIOVANNA PACCHIANA PARRAVICINI</span>, Presidente AGI Piemonte Valle d'Aosta</p>
+            <p class="font-Roboto font-thin text-xl italic text-white text-left "><span class="font-bold">Dott.ssa DANIELA PALIAGA,</span>, Presidente Centro Studi Sezione Piemonte e Presidente della sezione lavoro del Tribunale di Torino</p> -->
+            <!-- <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 12.30 - 12.45 </p>
+            <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Prenotazione interventi </p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 12.45 - 13.45 </p>
+            <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Light lunch</p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 14</p>
+            <p class="font-Roboto font-thin text-2xl italic text-white text-left sm:pb-14"><span class="font-bold">Dott.ssa Lucia Tria</span>, Presidente di sezione Corte di Cassazione Sez. Lavoro “Malattia e disabilità del lavoratore davanti alla Corte di Cassazione”</p> -->
+        </div>
+        <!-- <div class="sm:px-14">
 
             <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 14.45</p>
             <p class="font-Roboto font-bold text-3xl italic text-white text-left sm:pb-14">Dibattito e interventi programmati</p>
@@ -43,7 +71,7 @@ Via Santa Maria n. 1 - Torino</p>
             <p class="font-Roboto font-thin text-2xl italic text-white text-left">Ore 17.00</p>
             <p class="font-Roboto font-bold text-3xl italic text-white text-left">Fine dei lavori</p>
            
-        </div>
+        </div> -->
             
     </div>
     </div>
