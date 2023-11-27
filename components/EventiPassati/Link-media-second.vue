6d9d38449fb006c18e4dbf209bb4ec78800a7edc
Homeyra-Mahmoudi <homeyra.mahmoudi@mail.polimi.it>
2023-11-27 11:52:05 +0100

    Modifying the links

--- components/EventiPassati/Link-media-second.vue
+++ components/EventiPassati/Link-media-second.vue
@@ -1,11 +1,12 @@
 <template>
+    <div>
     <div  class="lg:container mx-auto  pl-38 pr-38 ">
-        <div  class="grid lg:grid-cols-3 gap-10 bg-light-blue-row bg-opacity-50 px-4 py-6">
+        <div  class="grid lg:grid-cols-3 gap-10 bg-light-blue-row bg-opacity-50 px-14 py-6">
             <div  class="bg-blue-950 rounded-lg pl-4 pt-2">
                 <p class="font-Roboto lg:text-xl text-white font-normal">Archivio di diapositive </p>
                 <ul class="list-image-[url(/EventiPassatii/Presentation-white.svg)] list-inside pl-2 py-4" >
-                    <a  href="https://www.temporary-url.com/404.php" target="_blank"><li class="font-Roboto lg:text-l text-white font-normal hover:text-sky-500">RIFORMA CARTABIA E RIFLESSI SUL PROCESSO DEL LAVORO - Avv. prof. Giorgio Frus</li></a>
-                    <a href="https://www.temporary-url.com/404.php" target="_blank"><li class="font-Roboto lg:text-l text-white font-normal hover:text-sky-500">RIFORMA CARTABIA E RIFLESSI SUL PROCESSO DEL LAVORO - Mauro Mollo</li></a>
+                    <a  href="https://mega.nz/#P!AgFkAFVFnFHoCHajMekN0xxep702KuHVo0UlX_GsDqk4n8To-RKKlQWg_aZq5L79RON12vhiZF9tx0Cte4YWczE8mL_4Ej6P_WerJLozOKDcAHSXFs9X_EIFIiun1GkcmZr0CPc2iiY" target="_blank"><li class="font-Roboto lg:text-l text-white font-normal hover:text-sky-500">RIFORMA CARTABIA E RIFLESSI SUL PROCESSO DEL LAVORO - Avv. prof. Giorgio Frus</li></a>
+                    <a href="https://mega.nz/#P!AgFhnHEFBMgDv_WBe1CAzjyDkgkRxYMi7v254fv_OkUem4nwmpivttVd-tPZTjlojxd3XvW11JzODcZxNXwL8wtKXtxjaA4HssxIqaQiERfPhTv1YXum_4UkvUYFukvaFKieWrD49Sw" target="_blank"><li class="font-Roboto lg:text-l text-white font-normal hover:text-sky-500">RIFORMA CARTABIA E RIFLESSI SUL PROCESSO DEL LAVORO - Mauro Mollo</li></a>
                 </ul>
             </div>
             <div  class="bg-blue-950 hidden rounded-lg pl-4 pt-2">
@@ -19,6 +20,7 @@
                 </ul>
             </div>
         </div>
+    </div>
     </div>
 </template>
 
